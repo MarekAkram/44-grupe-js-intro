02112d8eff070593d75467ca5ffc00e0cffe9cea
Marek <sincereakram@gmail.com>
2023-04-27 10:24:00 +0300

    Numbers

---
+++ index.js
@@ -0,0 +1,16 @@
+// single-line comment
+// single-line comment
+// single-line comment
+
+/*
+Multi
+line
+comment
+*/
+
+// Baziniu konceptu:
+// 1) Kintamieji
+// 2) Salygos (if)
+// 3) Ciklai (for)
+// 4) Funkcijos
+// 5) Events (narsykles puseje)
---
+++ js/01-console.js
@@ -0,0 +1,11 @@
+console.clear();
+
+/*
+CONSOLE - spaisdiname norima rezultata/-us
+*/
+
+console.log(1);
+console.log(2);
+console.log(3, 4, 5);
+
+console.warn(1);
