4a98c5679e9a1ec8272dddd94cf126096dcca49a
Marek <sincereakram@gmail.com>
2023-05-03 15:54:19 +0300

    array method

--- js/01-console.js
+++ js/01-console.js
@@ -1,5 +1,4 @@
 console.clear();
-
 /*
 CONSOLE - spausdiname norima rezultata/-us
 */
@@ -9,3 +8,9 @@ console.log(2);
 console.log(3, 4, 5);
 
 console.warn(1);
+console.warn(2);
+console.warn(3, 4, 5);
+
+console.error(1);
+console.error(2);
+console.error(3, 4, 5);
